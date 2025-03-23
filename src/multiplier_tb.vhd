2586cee165a152231514e9d4e9a5d75ceb44504e
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-03-23 15:19:41 -0300

    Add testbench for multiplier component

---
+++ src/multiplier_tb.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity multiplier_tb is end;
+
+architecture behavior of multiplier_tb is
+    signal mtpr : std_logic_vector(6 downto 0);
+    signal mtpcd : std_logic_vector(31 downto 0);
+    signal product : std_logic_vector(38 downto 0);
+
+    component multiplier is
+        port (
+            mtpr    : in std_logic_vector(6 downto 0);
+            mtpcd   : in std_logic_vector(31 downto 0);
+            product : out std_logic_vector(38 downto 0)
+        );
+    end component;
+
+begin
+    mult : multiplier port map (mtpr, mtpcd, product);
+
+    process
+
+    begin
+        mtpr <= "0000100";
+        mtpcd <= "00000000000000010000000000000000";
+        wait for 10 ns;
+
+        wait;
+
+    end process;
+
+
+end architecture;
