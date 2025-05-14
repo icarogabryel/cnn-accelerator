2464190fa796c46eab0c096d5716960a7f81b86a
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-05-14 15:36:19 -0300

    refactor: update multiplier testbench to use consistent signal sizes and improve clarity

--- src/multiplier_tb.vhd
+++ src/multiplier_tb.vhd
@@ -6,51 +6,44 @@ entity multiplier_tb is
 end;
 
 architecture behavior of multiplier_tb is
-    signal clk, rst : std_logic;
-    signal clk_period : time := 10 ns;
+    signal clk, rst   : std_logic;
+    signal clk_period : time    := 10 ns;
     signal clk_count  : integer := 0;
 
-    signal mtpr    : std_logic_vector(6 downto 0);
-    signal mtpcd   : std_logic_vector(31 downto 0);
-    signal product : std_logic_vector(38 downto 0);
+    signal mtpr  : std_logic_vector(7 downto 0);
+    signal mtpcd : std_logic_vector(7 downto 0);
+    signal prod  : std_logic_vector(15 downto 0);
 
     component multiplier is
         port(
             clk, rst : in  std_logic;
-            mtpr     : in  std_logic_vector(6 downto 0);
-            mtpcd    : in  std_logic_vector(31 downto 0);
-            product  : out std_logic_vector(38 downto 0)
+            mtpr     : in  std_logic_vector(7 downto 0);
+            mtpcd    : in  std_logic_vector(7 downto 0);
+            prod     : out std_logic_vector(15 downto 0)
         );
     end component;
 
 begin
     mult : multiplier
         port map(
-            clk     => clk,
-            rst     => rst,
-            mtpr    => mtpr,
-            mtpcd   => mtpcd,
-            product => product
+            clk   => clk,
+            rst   => rst,
+            mtpr  => mtpr,
+            mtpcd => mtpcd,
+            prod  => prod
         );
 
-    clk_process: process
+    clk_process : process
     begin
         clk <= '0';
-        wait for clk_period/2;
+        wait for clk_period / 2;
 
-        clk <= '1';
+        clk       <= '1';
         clk_count <= clk_count + 1;
-        wait for clk_period/2;
-
-        if (clk_count = 10) then
-            report "ending simulation";
-            wait;
-
-        end if;
-
+        wait for clk_period / 2;
     end process clk_process;
 
-    rst_process: process
+    rst_process : process
     begin
         rst <= '0';
         wait for 2 ns;
@@ -60,23 +53,37 @@ begin
 
         rst <= '0';
         wait;
-
     end process rst_process;
 
-    data_process: process
+    data_process : process
     begin
         wait for 5 ns;
 
-        mtpr  <= std_logic_vector(to_signed(8, 7));
-        mtpcd <= std_logic_vector(to_signed(2, 32));
+        mtpr  <= std_logic_vector(to_signed(8, 8));
+        mtpcd <= std_logic_vector(to_signed(2, 8));
+
+        assert prod = std_logic_vector(to_signed(16, 16));
         wait for 10 ns;
 
-        mtpr  <= std_logic_vector(to_signed(-5, 7));
-        mtpcd <= std_logic_vector(to_signed(20, 32));
+        mtpr  <= std_logic_vector(to_signed(-5, 8));
+        mtpcd <= std_logic_vector(to_signed(3, 8));
+
+        assert prod = std_logic_vector(to_signed(-15, 16));
         wait for 10 ns;
 
+        mtpr  <= std_logic_vector(to_signed(-2, 8));
+        mtpcd <= std_logic_vector(to_signed(-5, 8));
+
+        assert prod = std_logic_vector(to_signed(10, 16));
+        wait for 10 ns;
+
+        mtpr  <= (others => '0');
+        mtpcd <= (others => '0');
+
+        assert prod = (others => '0');
+        wait for 10 ns;
+
+        assert false report "Test completed" severity note;
         wait;
-
     end process;
-
 end architecture;
