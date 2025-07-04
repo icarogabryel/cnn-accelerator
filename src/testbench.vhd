f1e8257ce7fc3b52a586639ed15b2d3a5ed2e46c
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-07-04 17:31:38 -0300

    fix: update assertion message in testbench for clarity

--- src/testbench.vhd
+++ src/testbench.vhd
@@ -32,7 +32,7 @@ begin
                 signed_value   <= std_logic_vector(to_signed(i, 8));
                 unsigned_value <= std_logic_vector(to_unsigned(j, 8));
                 wait for 10 ns;
-                assert result = std_logic_vector(to_signed(i * j, 16)) report "Test failed for mtpr=" & integer'image(i) & " mtpcd=" & integer'image(j) severity note;
+                assert result = std_logic_vector(to_signed(i * j, 16)) report "Test failed for signed_value=" & integer'image(i) & " unsigned_value=" & integer'image(j) severity note;
             end loop;
         end loop;
         wait;
